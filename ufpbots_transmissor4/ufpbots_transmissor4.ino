189f159dd21f46ff0518251f8976b3570229df2a
Vitor Rodrigues <31389443+vitorjrodrigues@users.noreply.github.com>
2018-08-24 19:31:42 -0300

    Add files via upload

---
+++ ufpbots_transmissor4/ufpbots_transmissor4.ino
@@ -0,0 +1,25 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#include "config.h"
+
+// declare variables
+RF24 radio(CE, CSN); //ce,csn pin
+uint8_t payload[10];
+
+void setup() {
+  //TransmitterConfiguration();
+  radio.begin();
+  radio.stopListening();
+  radio.openWritingPipe(add1);
+}
+
+void loop() {
+  while(Serial.available())
+  {
+    memset(payload, 0, sizeof(payload));
+    Serial.readBytesUntil('\n', payload, sizeof(payload));
+    radio.write(payload, sizeof(payload));
+  }
+}
