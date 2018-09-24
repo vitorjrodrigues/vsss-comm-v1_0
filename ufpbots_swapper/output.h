061ad6f5bea6318eb07b20aec975ac61a1c60f27
Vitor Rodrigues <31389443+vitorjrodrigues@users.noreply.github.com>
2018-09-24 19:12:17 -0300

    Add files via upload

--- ufpbots_swapper/output.h
+++ ufpbots_swapper/output.h
@@ -1,47 +1,33 @@
-#define PWM4  ((input[3]!=0 || input[4]!=0 || input[5]!=0 || input[6]!=0 || input[7]!=0 || input[8]!=0 || input[9]!=0 ))
-#define PWM3  ((input[0] >= '0' && input[0] <= '9') && (input[1] >= '0' && input[1] <= '9') && (input[2] >= '0' && input[2] <= '9'))
-#define PWM2  ((input[0] >= '0' && input[0] <= '9') && (input[1] >= '0' && input[1] <= '9') && !(input[2] >= '0' && input[2] <= '9'))
-#define PWM1  ((input[0] >= '0' && input[0] <= '9') && !(input[1] >= '0' && input[1] <= '9') && !(input[2] >= '0' && input[2] <= '9'))
+uint8_t PWMval = PWMMin;
+int v = 12;
 
 void printOutput(uint8_t output[])
 {
-  Serial.print("Sent:  [[ ");
-  for (uint8_t i = 0; i < 10; i++)
+  Serial.print("Sent: ");
+  for (uint8_t i = 1; i < 4; i++)
   {
     Serial.print(output[i]);
-    if (i < 9)
+    if (i < 3)
       Serial.print(", ");
-    else
-      Serial.println(" ]]");
   }
+  Serial.println();
 }
 
 void swap() {
-  int v = 0;
-  delay(250);
-  if(swapper>3) {
-    swapper = 0;
-  }
-  if(swapper ==0) {
-    v = 12;
-  }
-  else if(swapper ==1) {
-    v = 18;
-  }
-  else if(swapper ==2) {
-    v = 20;
-  }
-  else if(swapper ==3) {
-    v = 10;
-  }
-    output[1] = v;
-    output[4] = v;
-    output[7] = v;
+  if(swapper >3) swapper = 0;
+  if(swapper ==0) v = 12;
+  else if(swapper ==1) v = 18;
+  else if(swapper ==2) v = 20;
+  else if(swapper ==3) v = 10;
+  output[1] = v;
+  output[4] = v;
+  output[7] = v;
+  swapper++;
 }
 
 void increasePWM() {
   PWMval++;
-  if(PWMval>254){ PWMval = 0; swap(); }
+  if(PWMval>PWMMax){ PWMval = 10; swap(); }
   output[2] = PWMval;
   output[3] = PWMval;
   output[5] = PWMval;
--- ufpbots_swapper/ufpbots_swapper.ino
+++ ufpbots_swapper/ufpbots_swapper.ino
@@ -3,18 +3,20 @@
 #include <RF24.h>
 
 // declare variables
-RF24 radio(2, 3); //ce,csn pin
+RF24 radio(9, 10); //ce,csn pin
 const uint64_t add1 = 0x0a0c0a0c0aLL;
 uint8_t output[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-uint8_t zero[10] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-uint8_t PWMval = 0;
 int swapper = 0;
+
+#define PWMMin 30
+#define PWMMax 254
 
 #include "output.h"
 
 void setup() {
   Serial.begin(38400);
   radio.begin();
+  radio.setPayloadSize(10);
   radio.stopListening();
   radio.openWritingPipe(add1);
   Serial.println("|>PWM Swapper Ready<|");
@@ -33,28 +35,41 @@ void loop() {
 void serialEvent()
 {
   memset(output, 0, sizeof(output));
+  radio.flush_tx();
   Serial.readBytesUntil('\n', output, sizeof(output));
-  if(output[0]!=0)
+  if (output[0] != 0)
   {
-  if (output[0] == '@')
-  {
-    output[0] = 0;
-    radio.write(zero, sizeof(zero));
-    radio.write(zero, sizeof(zero));
-    radio.write(zero, sizeof(zero));
-    radio.write(zero, sizeof(zero));
-    radio.write(zero, sizeof(zero));
-    radio.write(zero, sizeof(zero));
-    Serial.println("\nHalted");
+    if (output[0] == '@')
+    {
+      memset(output, 0, sizeof(output));
+      radio.write(output, sizeof(output));
+      radio.flush_tx();
+      memset(output, 0, sizeof(output));
+      radio.write(output, sizeof(output));
+      radio.flush_tx();
+      memset(output, 0, sizeof(output));
+      radio.write(output, sizeof(output));
+      radio.flush_tx();
+      memset(output, 0, sizeof(output));
+      radio.write(output, sizeof(output));
+      radio.flush_tx();
+      memset(output, 0, sizeof(output));
+      radio.write(output, sizeof(output));
+      radio.flush_tx();
+      memset(output, 0, sizeof(output));
+      radio.write(output, sizeof(output));
+      radio.flush_tx();
+      Serial.println("\nHalted");
+    }
+    else if (output[0] == '!')
+    {
+      output[0] = 255;
+      Serial.println("\nStarted/Resumed");
+      swap();
+    }
+    else
+    {
+      Serial.print("\nCommand not detected");
+    }
   }
-  else if (output[0] == '!')
-  {
-    output[0] = 255;
-    Serial.println("\nStarted/Resumed");
-  }
-  else
-  {
-    Serial.print("\nCommand not detected");
-  }
-}
 }
